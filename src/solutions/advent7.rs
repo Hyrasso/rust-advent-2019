22eafb6da480683119e3e0222a4700d33552531e
Grégoire de Massé <demasse.gregoire@yahoo.fr>
2019-12-07 13:48:34 +0100

    Day 7 part 2 : 69113332
    cargo run -- -d 7 | sort
    TODO: get output from run another way

--- src/solutions/advent7.rs
+++ src/solutions/advent7.rs
@@ -2,7 +2,8 @@
 use std::fs;
 use crate::solutions::Solution;
 use itertools::Itertools;
-
+use std::thread;
+use std::sync::mpsc;
 pub struct Day7 {}
 
 fn get_addr(mode:i32, ip: usize, value: i32) -> usize {
@@ -13,11 +14,10 @@ fn get_addr(mode:i32, ip: usize, value: i32) -> usize {
     }
 }
 
-fn run(text: &str, input: Vec<i32>) -> i32 {
+fn run(text: &str, input: mpsc::Receiver<i32>, output: mpsc::Sender<i32>) {
+    // println!("Starting amp");
     let mut program: Vec<_> = text.split(",").map(|s| s.parse::<i32>().unwrap()).collect();
     let mut ip = 0;
-    let mut res = 0;
-    let mut ii = 0;
     loop {
         let op = program[ip] % 100;
         // eprintln!("D {}: {}", ip, op);
@@ -38,15 +38,17 @@ fn run(text: &str, input: Vec<i32>) -> i32 {
             // Get input -> a
             3 => {
                 let idest = get_addr(mode, ip + 1, program[ip + 1]);
-                program[idest] = input[ii];
-                ii += 1;
+                program[idest] = input.recv().unwrap();
                 ip += 2;
             },
             // set output -> a 
             4 => {
                 let idest = get_addr(mode, ip + 1, program[ip + 1]);
                 // eprint!("{} ", program[idest]);
-                res = program[idest];
+                let res = output.send(program[idest]);
+                if res.is_err() {
+                    println!("{}", program[idest]);
+                }
                 ip += 2;
             },
             // JNZ set ip -> b if a is non zero
@@ -92,24 +94,80 @@ fn run(text: &str, input: Vec<i32>) -> i32 {
             }
         }
     }
-    return res;
+    let res = input.try_recv();
+    if res.is_ok() {
+        println!("{}", res.unwrap());
+    }
 }
 
 impl Solution for Day7 {
     fn solve(&self) {
         let text = fs::read_to_string("inputs/7-1.txt").expect("Error while opening input");
-        // run(text, 1);
-        let mut max_sig = 0;
-        for perm in (0..5).permutations(5) {
-            let mut r = run(&text, vec![perm[0], 0]);
-            r = run(&text, vec![perm[1], r]);
-            r = run(&text, vec![perm[2], r]);
-            r = run(&text, vec![perm[3], r]);
-            r = run(&text, vec![perm[4], r]);
-            if r >= max_sig {
-                max_sig = r;
-            }
+        for perm in (5..10).permutations(5) {
+            let mpsc0 = mpsc::channel();
+            let mpsc1 = mpsc::channel();
+            let mpsc2 = mpsc::channel();
+            let mpsc3 = mpsc::channel();
+            let mpsc4 = mpsc::channel();
+            // phase config
+            mpsc0.0.send(perm[0]).unwrap();
+            mpsc1.0.send(perm[1]).unwrap();
+            mpsc2.0.send(perm[2]).unwrap();
+            mpsc3.0.send(perm[3]).unwrap();
+            mpsc4.0.send(perm[4]).unwrap();
+            // init input
+            let itx0 = mpsc::Sender::clone(&mpsc0.0);
+            itx0.send(0).unwrap();
+            let mut progs = vec![];
+            // Amp A
+            let instr = text.clone();
+            let irx = mpsc0.1;
+            let otx = mpsc1.0;
+            progs.push(thread::spawn(move || {
+                run(&instr, irx, otx);
+            }));
+            // Amp B
+            let instr = text.clone();
+            let irx = mpsc1.1;
+            let otx = mpsc2.0;
+            progs.push(thread::spawn(move || {
+                run(&instr, irx, otx);
+            }));
+            // Amp C
+            let instr = text.clone();
+            let irx = mpsc2.1;
+            let otx = mpsc3.0;
+            progs.push(thread::spawn(move || {
+                run(&instr, irx, otx);
+            }));
+            // Amp D
+            let instr = text.clone();
+            let irx = mpsc3.1;
+            let otx = mpsc4.0;
+            progs.push(thread::spawn(move || {
+                run(&instr, irx, otx);
+            }));
+            // Amp E
+            let instr = text.clone();
+            let irx = mpsc4.1;
+            let otx = mpsc0.0;
+            progs.push(thread::spawn(move || {
+                run(&instr, irx, otx);
+            }));
+            progs.into_iter().for_each(|t| t.join().unwrap());
         }
-        println!("{}", max_sig);
+        // let mut max_sig = 0;
+        // for perm in (5..10).permutations(5) {
+        //     let (tx, rx) = mpsc::channel();
+        //     let mut r = run(&text, vec![perm[0], 0]);
+        //     r = run(&text, vec![perm[1], r]);
+        //     r = run(&text, vec![perm[2], r]);
+        //     r = run(&text, vec![perm[3], r]);
+        //     r = run(&text, vec![perm[4], r]);
+        //     if r >= max_sig {
+        //         max_sig = r;
+        //     }
+        // }
+        // println!("{}", max_sig);
     }
 }
